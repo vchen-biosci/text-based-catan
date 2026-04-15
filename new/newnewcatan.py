b543838bf940a07dcc542a3385570faca73aa8ea
Vivienne Chen <vchen@kellettschool.com>
2026-04-15 20:26:23 +0800

    The reason the program is broken is i needed to quickly escape the loop to publish this. just remove the issue on line 417 and ull be find

--- new/newnewcatan.py
+++ new/newnewcatan.py
@@ -1,6 +1,7 @@
-
+import random, os, time
 
 def infinite_rng(game, CONSTS):
+        
         
         possible_stat_commands = ["cache", "mode", "mean", "num", "?", "reset", "indie"]
         print("Ohohoho, it seems you wish to play infinite rng, hm?")
@@ -143,6 +144,193 @@ def infinite_rng(game, CONSTS):
                         print("(Erasing all your gambling records...)")
                         rng_loop = False
 
+def setup_player_dicts(game, CONSTS):
+
+        game = game
+
+        while True:
+                try:
+                        player_number = int(input("How many people are playing? :)\n> ").strip()) 
+                        if player_number in [3, 4]:
+                                break
+                        else: 
+                                print("You can only play with 3 or 4 people, sorry!")
+
+                except ValueError:
+                        print("Enter an integer 3 or 4 please.")
+                        
+        game["player_number"] = player_number
+        quick_key = []
+        for player in range(player_number):
+                quick_key.append(player + 1)
+        game["quick_key"] = quick_key
+
+        player_names = []
+        for i in range(player_number):
+                game[i + 1] = {}
+                valid_name = False
+                while not valid_name:
+
+                        player_name = input(f"What do you want to be called?, player {i + 1}?\n> ").strip()
+                        if player_name not in player_names:
+                                
+                                break
+
+                        else:
+                                print("Stop stealing another player's name!! Weirdo!!")
+
+                player_names.append(player_name)
+                game[i + 1]["name"] = player_name 
+        
+        game["player_names"] = player_names
+
+
+        return game
+
+def print_own_deck(game, CONSTS):
+        print("this is ur deck")
+
+def roll_die(game, CONSTS):
+
+
+        dice_1 = random.randint(1, 6)
+        dice_2 = random.randint(1, 6)
+        roll = dice_1 + dice_2
+        print(f"The die have spoken!! |{dice_1}| |{dice_2}| ... {dice_1} + {dice_2} = {roll}! You have rolled a {roll} :3")
+
+        return roll
+
+def setup_game(game, CONSTS):
+        print("Notice: While setting up the game, you temporarily can't use other commands.")
+        time.sleep(0.5)
+
+        game = setup_player_dicts(game, CONSTS)
+
+        print("Okay; your names are: ")
+        for player in range(game['player_number']):
+                print(game['player_names'][player], end="")
+                if player != 3:
+                        print(", ", end="")
+                else:
+                        print(".\n")
+        time.sleep(0.5)
+
+        game = assign_player_colours(game, CONSTS)
+
+        print("Initialising player cards...")
+        for player in game["quick_key"]:
+                for resource in CONSTS["resources"]:
+                        game[player][resource] = 0
+                for dev_card in CONSTS["dev_cards"]:
+                        game[player]["dev_cards"] = 0
+        time.sleep(0.3)
+
+        print("Setting up the resource bank...")
+        for resource in CONSTS["resources"]:
+                game[resource] = 19
+        for dev_card in CONSTS["dev_cards"]:
+                pass
+        time.sleep(0.3)
+
+        
+
+
+
+
+        return game
+
+def ansi_stitching(color : list, text : str):
+        
+        colored_ver = ""
+        colored_ver += "\x1b[38;2;"
+
+        reps = 0
+        for value in color:
+                colored_ver += str(value)
+                reps += 1
+
+                if reps != 3:
+                        colored_ver += ";"
+        
+        colored_ver += "m"
+        colored_ver += text
+        colored_ver += "\x1b[0m"
+
+        return colored_ver
+
+def choose_colours(CONSTS):
+
+        player_color = []
+
+        satisfied = False
+        while not satisfied:
+                for color in CONSTS["colors"]:                  
+                        
+                        valid_input = False
+                        while not valid_input:
+                                action = input(f"What value would you like to use for {color}?\n> ").strip().lower()
+
+                                try:
+                                        if int(action) <= 255:
+                                                player_color.append(int(action))
+                                                valid_input = True
+
+                                                
+                                        else:
+                                                print("Sorry; RGB values only go up to 255.")
+                                        
+
+                                except ValueError:
+
+                                        print("Please input a valid integer, in arabic numerals, within the range of 0 to 255.")     
+                                        
+                confirmed = False
+                while not confirmed:
+
+                        confirm = input(ansi_stitching(player_color, """This is what your colour looks like - are you sure you want it? Type 'Y' for yes and 'N' for no. 
+Please make sure all other players are able to read this!\n""") + "> ").strip()
+                        
+                        if confirm == "N":
+                                player_color = []
+                                confirmed = True
+                        
+                        elif confirm == "Y":
+                                satisfied = True
+                                confirmed = True
+                                
+                        else:
+                                print("Please type either 'Y' or 'N'. This is case sensitive.")
+
+        return player_color
+
+def assign_player_colours(game, CONSTS):
+
+
+        preset_colors = [[0, 201, 184], [252, 210, 0], [252, 84, 0], [210, 0, 252]]
+
+        manual = ""
+        while manual not in ["y", "m"]:
+
+                manual = input("Would you like to use our pre-selected, super aesthetic colours or customise your own? Please be responsible!!" +
+                        " Type 'Y' for yes and 'M' to manually select.\n> ").lower().strip()
+                
+        if manual == "m":
+
+                for player in range(game["player_number"]):
+                        print(f"OKAY! PLAYER {player + 1}, YOU'RE UP!!!")
+                        game[player + 1]["color"] = choose_colours(CONSTS)
+
+        else:
+                
+                for player in range(game["player_number"]):
+                        game[player+1]["color"] = preset_colors[player]
+
+        for player in range(game["player_number"]):
+                print(ansi_stitching(game[player + 1]["color"], f"Player {player + 1}, this is your colour."))
+                time.sleep(0.3)
+
+        return game
+
 def main():
         CONSTS = {
 
@@ -159,33 +347,34 @@ CREDITS: Vivienne, CATAN game studio
 ENTER YOUR COMMAND TO BEGIN :)""",
 
                 "pre_commands" : {
-                        "inf": display_info,
-                        "pcl": print_commands_list,
                         "rng": infinite_rng
 },
 
-                "commands" : {
-                        "pcl": print_commands_list, 
-                        "pbc": print_building_costs,
+                "commands" : { 
                         "pod": print_own_deck, 
-                        "inf": display_info, 
                         "roll": roll_die,
 },
         
 
                 "credits": """The credits for this code are as follows:""",
 
-                "commands info" : """These are the commands available to you and what they mean""" 
+
+                "commands info" : """These are the commands available to you and what they mean""",
+
+                "building costs" : "building costs are:",
+
+                "colors" : ["red", "green", "blue"]
 
         }
 
         game = {
-                "input type" : CONSTS["pre_commands"]
+                "input type" : CONSTS["pre_commands"],
+                "on" : True
         }
 
         print(CONSTS["welcome_message"])
 
-        while game["input type"] == "off":
+        while True:
         
                 action = input("> ").strip().lower()
 
@@ -201,8 +390,16 @@ ENTER YOUR COMMAND TO BEGIN :)""",
                 elif action == "pcl":
                         print(CONSTS["commands info"])
 
+                elif action == "info":
+                        print(CONSTS["info"])
+
+                elif action == "pbc":
+                        print(CONSTS["building costs"])
+
                 elif action == 'start game':
-                        print("\nStarting your game...\n")
+                        game["input type"] = CONSTS["commands"]
+                        print("Starting your game...")
+                        game = setup_game(game, CONSTS)
                         
                 else:
                         try:
@@ -217,4 +414,5 @@ ENTER YOUR COMMAND TO BEGIN :)""",
 
 
 if __name__== "__main__":
+        break program lowk
         main()
